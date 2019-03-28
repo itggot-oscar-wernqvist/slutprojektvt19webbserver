ae4c2f07c7f6c904a45a3a8e72ca495c75779da0
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2019-03-28 10:50:38 +0100

    påbörjat lite login och så

--- app.rb
+++ app.rb
@@ -6,6 +6,21 @@ require_relative('database.rb')
 enable :sessions
 
 get('/') do
-    
-
+    result = fetch_posts()
+    slim(:index, locals:{
+        posts: result
+    })
 end
+
+post('/login_attempt') do
+    if login_check(params["username"], params["password"]) == true
+        session[:logged_in] = true
+        session[:username] = get_user_id(params["username"])
+        session[:user_id] = user_id
+        redirect('/admin')
+    else
+        session[:logged_in] = false
+        redirect('/login')
+    end    
+end
+
--- database.rb
+++ database.rb
@@ -2,3 +2,17 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+def login_check(username, password)
+    db = SQLite3::Database.new("db/blog.db")
+    password_hash = db.execute("SELECT users.password_hash FROM users WHERE users.username = ?", username)
+    if password_hash.length > 0 && BCrypt::Password.new(password_hash[0][0]).==(password)
+        return true
+    else
+        return false
+    end
+end
+
+def get_user_id(username)
+    user_id = db.execute("SELECT users.user_id FROM users WHERE users.username = ?", username) 
+    return user_id
+end
