72063b02705991f8a194784ac94195630e844aa3
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2019-04-04 10:57:21 +0200

    upvotes och login

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'bcrypt'
 require_relative('database.rb')
 enable :sessions
 
+
 get('/') do
     result = fetch_posts()
     slim(:index, locals:{
@@ -15,12 +16,52 @@ end
 post('/login_attempt') do
     if login_check(params["username"], params["password"]) == true
         session[:logged_in] = true
-        session[:username] = get_user_id(params["username"])
-        session[:user_id] = user_id
-        redirect('/admin')
+        session[:username] = params["username"]
+        session[:user_id] = get_user_id(params["username"])
+        redirect('/')
     else
         session[:logged_in] = false
-        redirect('/login')
+        redirect('/')
     end    
 end
 
+post('/register_attempt') do
+    register(params["username"], params["password"])
+    redirect('/')
+end
+
+post('/post_post') do
+    post_post(params["title"],params["content"])
+    redirect('/')
+end
+
+get('/logout') do
+    session.clear
+    redirect('/')
+end
+
+get('/create_post') do
+    if session[:logged_in] == true
+        slim(:create)
+    else
+        redirect('/')
+    end
+end
+
+get('/upvote_post/:id') do
+    if prevoius_post_vote(params["id"], session[:user_id]) == false
+        vote_post(params["id"], session[:user_id], 1)
+        redirect('/')
+    else
+        redirect('/')
+    end
+end
+
+get('/downvote_post/:id') do
+    if prevoius_post_vote(params["id"], session[:user_id]) == false
+        vote_post(params["id"], session[:user_id], -1)
+        redirect('/')
+    else
+        redirect('/')
+    end
+end
--- database.rb
+++ database.rb
@@ -1,18 +1,51 @@
-require 'slim'
-require 'sqlite3'
-require 'bcrypt'
+
 
 def login_check(username, password)
-    db = SQLite3::Database.new("db/blog.db")
+    db = SQLite3::Database.new("db/reddit.db")
     password_hash = db.execute("SELECT users.password_hash FROM users WHERE users.username = ?", username)
     if password_hash.length > 0 && BCrypt::Password.new(password_hash[0][0]).==(password)
+        return true
+    end
+    return false
+end
+
+def get_user_id(username)
+    db = SQLite3::Database.new("db/reddit.db")
+    user_id = db.execute("SELECT users.user_id FROM users WHERE users.username = ?", username) 
+    return user_id
+end
+
+def fetch_posts()
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT posts.post_id, posts.title, posts.content, posts.timestamp, posts.user_id, users.username, posts.upvote_count, posts.image_link FROM posts INNER JOIN users ON users.user_id = posts.user_id").reverse
+    return result
+end
+
+def register(username,password)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", username, BCrypt::Password.create(password))
+end
+
+def post_post(title,content)
+    db = SQLite3::Database.new("db/reddit.db")
+    time = Time.now.asctime
+    db.execute("INSERT INTO posts (title, content, user_id, timestamp, upvote_count) VALUES (?,?,?,?,?)", title, content, session[:user_id], time, 0)
+end
+
+def prevoius_post_vote(post_id, user_id)
+    db = SQLite3::Database.new("db/reddit.db")
+    result = db.execute("SELECT post_upvotes.post_id, post_upvotes.user_id FROM post_upvotes WHERE post_upvotes.user_id = ? AND post_upvotes.post_id = ?", user_id, post_id) 
+    if result.length > 0
         return true
     else
         return false
     end
 end
 
-def get_user_id(username)
-    user_id = db.execute("SELECT users.user_id FROM users WHERE users.username = ?", username) 
-    return user_id
+def vote_post(post_id, user_id, value)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.execute("INSERT INTO post_upvotes (post_id, user_id) VALUES (?,?)", post_id, user_id)
+    upvote_count = db.execute("SELECT posts.upvote_count FROM posts WHERE posts.post_id = ?", post_id)[0][0].to_i
+    db.execute("UPDATE posts SET upvote_count = ? WHERE post_id = ?", upvote_count + value, post_id)
 end
